1a26cefffd65d3d0946c609438c5ec3cda4a6302
MinhTanVu <vuminhtan2810@gmail.com>
2024-04-19 12:51:15 +0700

    Another Branch

--- Controller.cpp
+++ Controller.cpp
@@ -1,11 +1,14 @@
 #include "Manager.h"
 #include "Display.h"
+#include "ManagerRepository.h"
 
 int main() {
     std::cout << "============= BRANCH FROM MAIN ===============" << std::endl;
     Display display;
+    ManagerRepository managerRepository;
     Manager manager(1,"Khanh",3500.5);
     display.showManagerInfo(manager);
+    managerRepository.removeManager(1);
     Engineer engineer1(1,"Tan",1000.5);
     Engineer engineer2(2,"Toan",1500.3);
     Engineer engineer3(3,"Tuan",1450.8);
